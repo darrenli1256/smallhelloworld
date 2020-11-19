f824bd366066adba67d414024bdfefbfc041e36b
Darren Li <occa8520@gmail.com>
2020-11-19 17:13:26 +0800

    2nd test

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 
 
 // test Docker
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
 
+app.get('/', (req ,res) => {
+    res.send('Hello, World!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+});
